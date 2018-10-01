66fe63cf82fb11c7a7b2021510af4e61620ddfef
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2018-10-01 13:13:46 +0200

    Switch to scripting API 1.3 and non-kts scripts

--- spek-script/jvm/src/main/kotlin/org/spekframework/spek2/script/SpekScript.kt
+++ spek-script/jvm/src/main/kotlin/org/spekframework/spek2/script/SpekScript.kt
@@ -3,27 +3,31 @@ package org.spekframework.spek2.script
 import org.spekframework.spek2.CreateWith
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.dsl.Root
+import org.spekframework.spek2.lifecycle.InstanceFactory
 import kotlin.reflect.KClass
-import kotlin.reflect.full.primaryConstructor
 import kotlin.script.experimental.annotations.KotlinScript
-import kotlin.script.experimental.annotations.KotlinScriptDefaultCompilationConfiguration
-import kotlin.script.experimental.annotations.KotlinScriptFileExtension
-import kotlin.script.experimental.api.*
-import kotlin.script.experimental.misc.*
-import kotlin.script.experimental.util.TypedKey
+import kotlin.script.experimental.api.ScriptCompilationConfiguration
+import kotlin.script.experimental.api.defaultImports
+import kotlin.script.experimental.api.implicitReceivers
 
-@KotlinScript
-@KotlinScriptFileExtension("spek.kts")
-@KotlinScriptDefaultCompilationConfiguration(SpekScriptConfiguration::class)
+@KotlinScript(fileExtension = "spek", compilationConfiguration = SpekScriptConfiguration::class)
 @CreateWith(SpekScriptFactory::class)
-open class SpekScript(spekScriptKlassToWrap: KClass<SpekScript>?, private val specProxy: DynamicSpecProxy) : Spek({
-    specProxy.proxy = this
-    spekScriptKlassToWrap?.primaryConstructor?.call(null, specProxy)
-}), Root by specProxy
+open class SpekScript(spekScriptClassToWrap: KClass<out SpekScript>?) : Spek({
+    if (spekScriptClassToWrap != SpekScript::class) {
+        spekScriptClassToWrap?.java?.constructors?.get(0)?.let {
+            it.newInstance(null, this)
+        }
+    }
+})
 
-object SpekScriptConfiguration : List<Pair<TypedKey<*>, Any?>> by ArrayList<Pair<TypedKey<*>, Any?>>(
-    listOf(
-        ScriptCompileConfigurationProperties.baseClass<SpekScript>(),
-        ScriptCompileConfigurationProperties.defaultImports("org.spekframework.spek2.dsl.*")
-    )
-)
+object SpekScriptConfiguration : ScriptCompilationConfiguration({
+    implicitReceivers(Root::class)
+    defaultImports("org.spekframework.spek2.dsl.*")
+})
+
+class SpekScriptFactory : InstanceFactory {
+    override fun <T : Spek> create(spek: KClass<T>): T {
+        val targetClass = spek as KClass<SpekScript>
+        return SpekScript(targetClass) as T
+    }
+}
